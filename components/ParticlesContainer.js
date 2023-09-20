08d27c2ab58283dfe0d137ce9146846959a5b8c7
Weslley Silva <weslley.the@gmail.com>
2023-09-20 16:49:11 -0300

    Finishing Particles

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -1,5 +1,100 @@
+import { Particles } from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import React, { useCallback } from 'react';
+
 const ParticlesContainer = () => {
-  return <div>Particles Container</div>;
+  // init particles
+  const initParticles = useCallback(async (tsParticles) => {
+    await loadFull(tsParticles);
+  }, []);
+
+  const particlesInit = useCallback(async () => { }, []);
+
+  const particlesLoaded = useCallback((container) => { }, []);
+
+  return (
+    <Particles className='w-full h-full absolute translate-z-0'
+      id='tsparticles'
+      init={initParticles}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: { enable: false },
+        background: {
+          color: {
+            value: ''
+          }
+        },
+        fps_limit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: false,
+              mode: 'push'
+            },
+            onHover: {
+              enable: true,
+              mode: 'repulse'
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 70
+            },
+            repulse: {
+              distance: 150,
+              duration: 0.4
+            }
+          }
+        },
+        particles: {
+          color: {
+            value: '#e68e2e'
+          },
+          links: {
+            color: '#f5d393',
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            with: 1
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            outMode: {
+              default: 'bounce',
+            },
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 800
+            },
+            value: 100
+          },
+          opacity: {
+            value: 0.5
+          },
+          shape: {
+            type: 'circle'
+          },
+          size: {
+            random: true,
+            value: 5
+          }
+        }
+      }}
+    />
+  );
+
+
+
 };
 
 export default ParticlesContainer;
--- pages/index.js
+++ pages/index.js
@@ -22,23 +22,47 @@ const Home = () => {
         <div className='text-center flex flex-col justify-center 
         xl:pt-40 xl:text-left h-full container mx-auto'>
           {/* title */}
-          <h1 className='h1'>
-            Teste de texto <br /> dentro do meu {' '} <span className='text-accent'>Portifolio</span>
-          </h1>
+          <motion.h1 variants={fadeIn('down', 0.2)}
+            initial='hidden' animate='show' exit='hidden'
+            className='h1'>
+            Seja bem vindo<br />ao meu{' '} <span className='text-accent'>Portfólio</span>
+          </motion.h1>
           {/* description */}
-          <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
+          <motion.p variants={fadeIn('down', 0.3)}
+            initial='hidden' animate='show' exit='hidden'
+            className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis
             urna fringilla, tempus nisl sit amet, varius ligula. Suspendisse
             hendrerit faucibus metus.
-          </p>
+          </motion.p>
           {/* button */}
           <div className='flex justify-center xl:hidden relative'>
             <ProjectsBtn />
           </div>
+          <motion.div variants={fadeIn('down', 0.4)}
+            initial='hidden' animate='show' exit='hidden'
+            className='hidden xl:flex'
+          >
+            <ProjectsBtn />
+          </motion.div>
         </div>
       </div>
       {/* image */}
-      <div>Image</div>
+      <div className='w-[1200px] h-full absolute right-0 bottom-0 '>
+        {/* bg image */}
+        <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right
+        xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'>
+        </div>
+        {/* particles */}
+        <ParticlesContainer />
+        {/* avatar */}
+        <motion.div variants={fadeIn('up', 0.5)}
+          initial='hidden' animate='show' exit='hidden' transition={{ duration: 1, ease: 'easeInOut' }}
+          className='w-full h-full max-w-[737px] max-h-[678px] 
+        absolute -bottom-32 lg:bottom-0 lg:right-[8%]'>
+          <Avatar />
+        </motion.div>
+      </div>
     </div>
   );
 };
