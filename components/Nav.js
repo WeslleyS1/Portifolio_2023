439ed245df4893441d9d32ff780ad7d99430c4b9
Weslley Silva <weslley.the@gmail.com>
2023-09-20 14:22:52 -0300

    Finishing Navigation

--- components/Nav.js
+++ components/Nav.js
@@ -34,7 +34,7 @@ import { useRouter } from 'next/router';
 
 const Nav = () => {
   const router = useRouter();
-  const {pathname} = router.pathname;
+  const pathname = router.pathname;
   return (
     <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 
     mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
@@ -43,7 +43,26 @@ const Nav = () => {
       gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 
       backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
         {navData.map((link, index) => {
-          return <Link href={link.path} key={index}>{link.icon}</Link>;
+          return (
+            <Link className={`${link.path === pathname && 'text-accent'} 
+            relative items-center group hover:text-accent trasition-all duration-300`}
+              href={link.path}
+              key={index}
+            >
+              {/* tooltip */}
+              <div className='absolute pr-14 right-0 hidden xl:group-hover:flex'>
+                <div className='bg-white relative flex text-primary items-center p-[6px] rounded-[3px]'>
+                  <div className='text-[12px] leading-none font-semibold capitalize'>
+                    {link.name}
+                  </div>
+                  {/* triangle */}
+                  <div></div>
+                </div>
+              </div>
+              {/* icon */}
+              <div>{link.icon}</div>
+            </Link>
+          );
         })}
       </div>
     </nav>
